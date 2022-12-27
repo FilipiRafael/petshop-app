ef778fd9c715082b048f27c4752ad56c7da09b25
Filipi Rafael <filipirafael.123@gmail.com>
2022-12-27 13:39:57 -0300

    Feature
    Add style for AppRoutes component

--- src/Components/TelaPadrao/index.js
+++ src/Components/TelaPadrao/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { SafeAreaView, KeyboardAvoidingView, Platform, StatusBar } from 'react-native';
 
-import estilos from '../../estilos';
+import estilosGlobais, { cores } from '../../estilos';
+import estilos from './estilos';
 
 export default function TelaPadrao({ children }) {
   return (
-    <SafeAreaView style={estilos.preencher}>
-      <StatusBar />
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-        style={estilos.preencher}
-      >
-      {children}
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={estilos.ajusteTela}>
+        <StatusBar backgroundColor={cores.roxo} />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+          style={estilosGlobais.preencher}
+        >
+        {children}
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+      <SafeAreaView style={estilos.ajusteTelaBaixo} />
+    </>
   )
 }
